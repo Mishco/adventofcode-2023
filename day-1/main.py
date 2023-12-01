4b93cf23840197eecd61a08b3bb184f6056366f1
misl <michal.slovik@tutanota.com>
2023-12-01 09:26:26 +0100

    Cleaning

--- day-1/main.py
+++ day-1/main.py
@@ -6,23 +6,20 @@ import re
 # a1b2c3d4e5f
 # treb7uchet
 # """
-#for line in test_input.splitlines():
-    # print(line)
+# for line in test_input.splitlines():
+# print(line)
 
-sum = 0
+sum = int(0)
 
 with open('input', mode='r') as f:
-    # all_lines = f.readlines()
-
     for line in f:
-    # for line in f.readlines():
-        print(line)
         m = re.search(r"\d", line)
         m2 = re.search(r"\d", line[::-1])
-        
+
         first_digit = line[m.start()]
         last_digit = line[::-1][m2.start()]
-        print(first_digit, last_digit)
-        sum += int(first_digit) + int(last_digit)
+        # print(first_digit, last_digit)
+        act_val = str(first_digit) + str(last_digit)
+        sum += int(act_val)
 
 print(sum)
