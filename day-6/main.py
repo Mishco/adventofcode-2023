ea1a0cfaade2c0f0339d18c6d70137c6fc913381
misl <michal.slovik@tutanota.com>
2023-12-06 13:50:44 +0100

    Add some tests

--- day-6/main.py
+++ day-6/main.py
@@ -1,14 +1,4 @@
-test = """
-Time:      7  15   30
-Distance:  9  40  200
-"""
-
 # and # 27102791
-
-f = open('../inputs/day06.txt', 'r')
-values = [x.strip("\r\n") for x in f.readlines()]
-times = map(int, values[0].split(':')[1].split())
-distances = map(int, values[1].split(':')[1].split())
 
 
 def calculate(times, dist):
@@ -22,9 +12,27 @@ def calculate(times, dist):
     return ret
 
 
-print(calculate(times, distances))  # 3316275 is right answer
+def run_tests():
+    test = """Time:      7  15   30
+    Distance:  9  40  200
+    """
+    val = [x.strip("\r\n") for x in test.splitlines()]
+    ttt = map(int, val[0].split(':')[1].split())
+    ddd = map(int, val[1].split(':')[1].split())
+    assert calculate(ttt, ddd) == 288, "should be 288"
 
-times = map(int, values[0].split(':')[1].replace(' ', '').split())
-distances = map(int, values[1].split(':')[1].replace(' ', '').split())
 
-print(calculate(times, distances))  # 27102791 is right
+if __name__ == '__main__':
+    run_tests()
+
+    f = open('../inputs/day06.txt', 'r')
+    values = [x.strip("\r\n") for x in f.readlines()]
+    times = map(int, values[0].split(':')[1].split())
+    distances = map(int, values[1].split(':')[1].split())
+
+    print(calculate(times, distances))  # 3316275 is right answer
+
+    times = map(int, values[0].split(':')[1].replace(' ', '').split())
+    distances = map(int, values[1].split(':')[1].replace(' ', '').split())
+
+    print(calculate(times, distances))  # 27102791 is right
